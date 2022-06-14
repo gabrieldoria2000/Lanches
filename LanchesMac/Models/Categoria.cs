300f50e10c507f177af4bbdd1e51a1fb2f245efb
gabrieldoria2000 <gabrieldoria2000@gmail.com>
2022-06-13 22:08:37 -0300

    primeiro commit

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage ="Informe o nome da categoria")]
+        [StringLength(100,ErrorMessage ="O tamanho máximo é {1} caracteres")]
+        [Display(Name ="Nome")]
+        public string CategoriaNome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo é {1} caracteres")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        //DEFINO O RELACIONAMENTO 1 X N ENTRE CATEGORIA E LANCHES
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
